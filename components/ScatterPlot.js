6d85797eb2e3c6d9f78301627f96ec663134b1d9
daviddoswell <team@daviddoswell.com>
2023-06-29 17:26:33 -0400

    add curve

--- components/ScatterPlot.js
+++ components/ScatterPlot.js
@@ -1,33 +1,63 @@
 import React, { useRef, useEffect } from "react";
-import * as d3 from "d3";
+import {
+  select,
+  scaleLinear,
+  max,
+  axisBottom,
+  axisLeft,
+  bin as d3Bin,
+  mean,
+} from "d3";
+
+function kdeKernelEpanechnikov(k) {
+  return function (v) {
+    return Math.abs((v /= k)) <= 1 ? (0.75 * (1 - v * v)) / k : 0;
+  };
+}
+
+function kde(kernel, thresholds) {
+  return function (values) {
+    return thresholds.map((t) => [t, mean(values, (d) => kernel(t - d))]);
+  };
+}
 
 const ScatterPlot = ({ data, width = 500, height = 500, padding = 50 }) => {
   const ref = useRef();
-
   const additionalPadding = 30;
 
   useEffect(() => {
-    const svg = d3.select(ref.current);
+    const svg = select(ref.current);
     svg.selectAll("*").remove();
+
+    const maleScores = data
+      .filter((d) => d.gender === "male")
+      .map((d) => d.score);
+    const femaleScores = data
+      .filter((d) => d.gender === "female")
+      .map((d) => d.score);
+
+    const kernel = kdeKernelEpanechnikov(7);
+    const thresholds = d3Bin().thresholds(maleScores.length)(maleScores);
+    const kdeEstimator = kde(kernel, thresholds);
+    const maleDensity = kdeEstimator(maleScores);
+    const femaleDensity = kdeEstimator(femaleScores);
+
+    // Your density drawing logic here...
 
     const innerWidth = width - 2 * padding;
     const innerHeight = height - 2 * padding;
 
-    const xScale = d3
-      .scaleLinear()
+    const xScaleScatter = scaleLinear()
       .domain([25, 105])
       .range([0, innerWidth])
       .nice();
 
-    const yScale = d3
-      .scaleLinear()
-      .domain([0, d3.max(data, (d) => d.score)])
+    const yScaleScatter = scaleLinear()
+      .domain([0, max(data, (d) => d.score)])
       .range([innerHeight, 0]);
 
-    console.log(data, xScale, yScale); // Log data and scales
-
-    const xAxis = d3.axisBottom(xScale);
-    const yAxis = d3.axisLeft(yScale).ticks(8);
+    const xAxis = axisBottom(xScaleScatter);
+    const yAxis = axisLeft(yScaleScatter).ticks(8);
 
     const g = svg
       .append("g")
@@ -43,8 +73,8 @@ const ScatterPlot = ({ data, width = 500, height = 500, padding = 50 }) => {
       .data(data)
       .enter()
       .append("circle")
-      .attr("cx", (d) => xScale(d.age))
-      .attr("cy", (d) => yScale(d.score))
+      .attr("cx", (d) => xScaleScatter(d.age))
+      .attr("cy", (d) => yScaleScatter(d.score))
       .attr("r", 5)
       .style("fill", (d) => {
         if (d.gender) {
@@ -53,7 +83,7 @@ const ScatterPlot = ({ data, width = 500, height = 500, padding = 50 }) => {
           return "#808080"; // Grey for undefined gender
         }
       });
-      
+
     svg
       .append("text")
       .attr("transform", `translate(${width / 2}, ${height - 5})`)
